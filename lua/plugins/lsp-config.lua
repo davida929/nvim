15a44df1ba853ca6cecf499080b1f2462c4ffe36
davida929 <davidabertho.93@gmail.com>
2024-09-05 22:55:55 +0100

    autocompletion done

--- lua/plugins/lsp-config.lua
+++ lua/plugins/lsp-config.lua
@@ -1,44 +1,66 @@
 return {
 	{
 		"williamboman/mason.nvim",
-		config = function() 
+		config = function()
 			require("mason").setup({
 				ui = {
 					icons = {
 						package_installed = "✓",
 						package_pending = "➜",
-						package_uninstalled = "✗"
-					}
-				}
+						package_uninstalled = "✗",
+					},
+				},
 			})
-		end
+		end,
 	},
 	{
 		"williamboman/mason-lspconfig.nvim",
-		config = function() 
-			require("mason-lspconfig").setup{
-				ensure_installed = { "lua_ls", "html", "tsserver"},
+		config = function()
+			require("mason-lspconfig").setup({
+				ensure_installed = {
+					"lua_ls",
+					"html",
+					"tsserver",
+					"clangd",
+				},
 				automatic_installation = true,
-			}
-		end
-	}, 
+			})
+		end,
+	},
 	{
 		"neovim/nvim-lspconfig",
-		config = function() 
+		config = function()
+			local capabilities = require("cmp_nvim_lsp").default_capabilities()
 			require("mason").setup()
 
 			-- configuring lsp-config
-			local lspconfig = require('lspconfig')
+			local lspconfig = require("lspconfig")
 
-			-- keymap bindings 
-			vim.keymap.set('n', 'K', vim.lsp.buf.hover, {} )
-			vim.keymap.set('n', 'gd', vim.lsp.buf.definition, {} )
-			vim.keymap.set('n', '<leader>ca', vim.lsp.buf.code_action, {} )
+			-- keymap bindings
+			vim.keymap.set("n", "K", vim.lsp.buf.hover, {})
+			vim.keymap.set("n", "gd", vim.lsp.buf.definition, {})
+			vim.keymap.set("n", "<leader>ca", vim.lsp.buf.code_action, {})
 
-
-			-- configuring server 
-			lspconfig.lua_ls.setup{}
-		end
-	}
-
+			-- configuring server
+			lspconfig.lua_ls.setup({
+				capabilities = cabalities,
+			})
+			-- configuring server
+			lspconfig.tsserver.setup({
+				capabilities = cabalities,
+			})
+			-- configuring server
+			lspconfig.html.setup({
+				capabilities = cabalities,
+			})
+			-- configuring server
+			lspconfig.dockerls.setup({
+				capabilities = cabalities,
+			})
+			-- configuring server
+			lspconfig.clangd.setup({
+				capabilities = cabalities,
+			})
+		end,
+	},
 }
