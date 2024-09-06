e74c4b0f7e2199c8cd146d876f480e5d0b77d47e
davida929 <davidabertho.93@gmail.com>
2024-09-06 05:00:47 +0100

    added cyberdream

--- lua/vim-options.lua
+++ lua/vim-options.lua
@@ -1,14 +1,14 @@
 -- basic indentation
 vim.cmd("set number")
 vim.cmd("set rnu")
-vim.cmd("set scrolloff=8")
+vim.cmd("set scrolloff=16")
 vim.cmd("set smartindent")
 vim.cmd("set smarttab")
 
 -- tabulation
 vim.cmd("set tabstop=4")
 vim.cmd("set softtabstop=4")
-vim.cmd("set shiftwidth=4")
+vim.cmd("set shiftwidth=2")
 
 -- basic keymap
 vim.keymap.set("n", "<C-s>", ":w<CR>", {})
